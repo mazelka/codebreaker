d922e71538cadc2238bb2333970eb7366380d123
Mary Banshee <mary.banshee@gmail.com>
2019-06-05 12:16:31 +0300

    add files to Codebreaker namespace

---
+++ lib/codebreaker.rb
@@ -0,0 +1,11 @@
+require 'codebreaker/version'
+require 'codebreaker/game'
+require 'codebreaker/game_statistic'
+require 'codebreaker/interface'
+require 'helpers/iohelper'
+require 'helpers/user'
+require 'helpers/statistic'
+
+module Codebreaker
+  class Error < StandardError; end
+end
